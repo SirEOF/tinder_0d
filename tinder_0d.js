afe0881f03443ad6b44fc6cb338605eddb007dd5
Leo Smith <k0oo@icloud.com>
2019-12-30 14:44:37 +0100

    :underage:

---
+++ tinder_0d.js
@@ -0,0 +1,6 @@
+setInterval(function() {
+ var element = document.getElementsByClassName("Bdrs(8px) Bgz(cv) Bgp(c) Ov(h) StretchedBox Ir(p) Cnt($blank)::a StretchedBox::a Bg($inherit)::a Scale(1.3)::a Scale(1.2)::a--s Blur(12px)::a");
+  for (i = 0; i < element.length; i++) {
+      element[i].className = "Bdrs(8px) Bgz(cv) Bgp(c) Ov(h) StretchedBox Ir(p) Cnt($blank)::a StretchedBox::a Bg($inherit)::a Scale(1.3)::a Scale(1.2)::a--s";
+  }
+}, 500);
